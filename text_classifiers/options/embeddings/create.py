bc751264fef34b8e5a2a0782329fcc9d09d9204c
Natalia Bukarina <n.tash92@gmail.com>
2021-05-16 20:29:52 +0200

    Add a script to create word embeddings

---
+++ text_classifiers/options/embeddings/create.py
@@ -0,0 +1,30 @@
+import time 
+
+import pandas as pd
+import gensim
+
+from text_classifiers.helpers import preprocess_corpus, PreprocessedCorpus 
+
+
+def run():
+    print("Reading in data...")
+    articles1 = pd.read_csv("data/raw/articles1.csv")
+    articles2 = pd.read_csv("data/raw/articles2.csv")
+    articles3 = pd.read_csv("data/raw/articles3.csv")
+    all_articles = pd.concat([articles1, articles2, articles3])
+
+    all_content = all_articles["content"]
+
+    print("Preprocessing corpus...")
+    start_time = time.time()
+    sentences = PreprocessedCorpus(all_content)
+    print(f"--- Took {round(time.time() - start_time, 2)} seconds ---")
+
+    print("Training the embeddings model...")
+    start_time = time.time()
+    model = gensim.models.Word2Vec(sentences=sentences)
+    print(f"--- Took {round(time.time() - start_time, 2)} seconds ---")
+
+    embed_path = "data/trained_models/embeddings.wordvec"
+    print("Saving the model to ", embed_path)
+    model.wv.save(embed_path)
